c67b20bea6c3478480ba297f13b736dc46ca2019
Abraham Pineda Zelaya <abrahampcr@gmail.com>
2019-09-03 15:53:56 -0600

    Adding basic function

--- main.ts
+++ main.ts
@@ -1,1 +1,32 @@
- 
+// Here are the different types of blocks
+// supported by MakeCode
+
+//% color="#4C97FF"
+namespace follower {
+
+    /**
+     * This is a statement block with a parameter
+     */
+    //% block
+    export function move(Hero: any, Villain: any, vSpeed: number) {
+        if (Hero.x > Villain.x) {
+            Villain.vx = vSpeed
+        } else {
+            Villain.vx = -1 * vSpeed
+        }
+        if (Hero.y > Villain.y) {
+            Villain.vy = vSpeed
+        } else {
+            Villain.vy = -1 * vSpeed
+        }
+    }
+
+
+    /**
+     * This is an event handler block
+     */
+    //% block="on event"
+    export function onEvent(handler: () => void) {
+
+    }
+}
