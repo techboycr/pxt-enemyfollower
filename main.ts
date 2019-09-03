e7bbe3615d48bd07cc8f6c608ff55037b45d01d5
Abraham Pineda Zelaya <abrahampcr@gmail.com>
2019-09-03 16:15:01 -0600

    Updates.

--- main.ts
+++ main.ts
@@ -8,21 +8,22 @@ namespace follower {
      * This is a statement block with a parameter
      */
     //% block
+    export function follower(Hero: Sprite, Villain: Sprite, vSpeed: number) {
+        loops.forever(function () {
+            if (Hero.x > Villain.x) {
+                Villain.vx = vSpeed
 
-    loops.forever(function (Hero: Sprite, Villain: Sprite, vSpeed: number) {
-        if (Hero.x > Villain.x) {
-            Villain.vx = vSpeed
-
-            game.splash("Hero.x")
-        } else {
-            Villain.vx = -1 * vSpeed
-        }
-        if (Hero.y > Villain.y) {
-            Villain.vy = vSpeed
-        } else {
-            Villain.vy = -1 * vSpeed
-        }
-    })
+                game.splash("Hero.x")
+            } else {
+                Villain.vx = -1 * vSpeed
+            }
+            if (Hero.y > Villain.y) {
+                Villain.vy = vSpeed
+            } else {
+                Villain.vy = -1 * vSpeed
+            }
+        })
+    }
 
     /**
      * This is an event handler block
