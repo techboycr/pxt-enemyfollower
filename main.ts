e29416c5129edd26fc7972c21f4e2017ca1bcadb
Abraham Pineda Zelaya <abrahampcr@gmail.com>
2019-09-04 10:46:31 -0600

    Changes better text

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ namespace follower {
     /**
      * This is a statement block with a parameter
      */
-    //% block="follower"
+    //% block
     export function follower(Hero: Sprite, Villain: Sprite, vSpeed: number) {
         loops.forever(function () {
             if (Hero.x > Villain.x) {
@@ -23,4 +23,3 @@ namespace follower {
         })
     }
 }
- 
