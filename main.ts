a910b584b4bd76d86b5ae0914931e1073a0d836f
Abraham Pineda Zelaya <abrahampcr@gmail.com>
2019-09-04 15:41:54 -0600

    Updates.

--- main.ts
+++ main.ts
@@ -9,7 +9,7 @@ namespace follower {
      * This is a statement block with a parameter
      */
     //% vSpeed.defl=5
-    //% block="Follow - Sprite %Villain=variables_get(mySprite)| will follow %Hero=variables_get(mySprite2) | at a speed of  %vSpeed"
+    //% block="Follow - Sprite $Villain=variables_get(mySprite)| will follow $Hero=variables_get(mySprite2) | at a speed of  %vSpeed"
     export function follow(Hero: any, Villain: any, vSpeed: number) {
         loops.forever(function () {
             if (Hero.x == Villain.x && Hero.y == Villain.y){
